09968c9a21b37333b007a6abf478ef6c9cf3317b
Adam Olley <adam.olley@blackboard.com>
2016-12-05 13:29:23 +1030

    search: Rename class being extended due to MDL-55278

--- classes/search/activity.php
+++ classes/search/activity.php
@@ -31,5 +31,5 @@ defined('MOODLE_INTERNAL') || die();
  * @package    mod_lightboxgallery
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class activity extends \core_search\area\base_activity {
+class activity extends \core_search\base_activity {
 }
