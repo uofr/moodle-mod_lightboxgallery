2425a71285a8cd4a81de203423e86925bab1f06b
Adam Olley <adam.olley@blackboard.com>
2018-06-27 09:40:02 +0930

    Release v3.3.0.6

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2018051500;  // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version  = 2018062700;  // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2017051500;  // Requires this Moodle version.
 $plugin->cron     = 0;           // Period for cron to check this plugin (secs).
 
 $plugin->component = 'mod_lightboxgallery';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.3.0.5';
+$plugin->release = '3.3.0.6';
